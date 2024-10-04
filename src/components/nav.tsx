6c3df1e2d29339d9ca27e0ca91403d562d2a3058
cxl2020MC <andamu2019@163.com>
2024-10-04 21:26:42 +0800

    更新了导航栏组件和样式，优化了页面布局和交互效果

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,13 +1,15 @@
-import Link from 'next/link'
-import { Icon } from '@iconify/react'
-import blogConfig from '@/blog.config'
-import "@/app/css/nav.css"
-import NavThemeToggleBtn from './themeToggleBtn'
+import Link from "next/link";
+import { Icon } from "@iconify/react";
+import blogConfig from "@/blog.config";
+import "@/app/css/nav.css";
+import NavThemeToggleBtn from "./themeToggleBtn";
 
 export default function Nav() {
   return (
     <nav className="nav">
-      <Link id="site-name" href="/">陈鑫磊的博客</Link>
+      <Link id="site-name" href="/">
+        陈鑫磊的博客
+      </Link>
       <div className="nav-links">
         <NavLinks />
       </div>
@@ -17,7 +19,7 @@ export default function Nav() {
         <NavMenuBtn icon="fa6-solid:bars" />
       </div>
     </nav>
-  )
+  );
 }
 
 function NavLinks() {
@@ -27,16 +29,14 @@ function NavLinks() {
         <Icon icon={item.icon}></Icon>
         <span>{item.name}</span>
       </Link>
-    )
-  })
+    );
+  });
 }
 
-
-function NavMenuBtn({ icon }: { icon: string, onClick?: () => void }) {
+function NavMenuBtn({ icon }: { icon: string; onClick?: () => void }) {
   return (
     <button className="nav-btn">
       <Icon icon={icon}></Icon>
     </button>
-  )
+  );
 }
-
