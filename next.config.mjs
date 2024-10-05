efb031231bc0b4b978379338885ad3d06f4dc4bd
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-10-05 13:16:44 +0000

    更新了博客配置，启用了MDX支持

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
+import createMDX from '@next/mdx'
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     images: {
         remotePatterns: [
             {
@@ -10,4 +14,11 @@ const nextConfig = {
     }
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+    // Add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
+
+// export default nextConfig;
