a052d288564741b8df7f77892db8817f4eff6ec5
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-10-06 11:18:36 +0000

    test

--- src/utils/renderMarkdown.ts
+++ src/utils/renderMarkdown.ts
@@ -1,4 +1,4 @@
-import rehypeRaw from 'rehype-raw'
+// import rehypeRaw from 'rehype-raw'
 import rehypeStringify from 'rehype-stringify'
 import remarkGfm from 'remark-gfm'
 // import remarkMath from 'remark-math'
@@ -15,8 +15,9 @@ export default async function renderMarkdown(markdown: string) {
     .use(remarkParse)
     .use(remarkGfm)
     // .use(remarkMath)
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
+    // .use(remarkRehype, { allowDangerousHtml: true })
+    // .use(rehypeRaw)
+    .use(remarkRehype)
     .use(rehypeShiki, {
         themes: {
             light: 'catppuccin-latte',
