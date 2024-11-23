4cee5a4de84d08b01189c08981d3071e577b8af2
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-11-23 05:24:07 +0000

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./css/globals.scss";
+import "./css/globals.css";
 import Nav from "@/components/nav";
 import Aside from "@/components/aside/aside";
 
