a0934fbb60d90f897ba5aae0e65c8836c5397219
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-11-23 07:40:58 +0000

    test

--- src/app/posts/[slug]/page copy.tsx
+++ src/app/posts/[slug]/page copy.tsx
@@ -2,6 +2,7 @@
 // import Link from "next/link";
 import { notFound } from "next/navigation";
 import blogConfig from "@/blog.config";
+import renderMarkdown from "@/utils/renderMarkdown";
 import "@/app/css/posts.css";
 import "@/app/css/markdown.css";
 
@@ -16,10 +17,15 @@ export default async function PostPage(props: { params: Promise<{ slug: string }
   }
   const md_content = post.data.md_content;
   console.log(md_content)
+  const md_html = await renderMarkdown(md_content);
+
+  console.log(md_html)
 
   return (
-    <div className="posts card">
-      <MDXRemote source={md_content} />;
+    <div className="posts card" dangerouslySetInnerHTML={{
+      __html: md_html
+    }
+    }>
     </div>
   )
 }
