45ce891f280a7f7f5f32d8299b6fab8f13656347
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2025-02-07 13:17:11 +0000

    test

--- src/components/markdown/pre.tsx
+++ src/components/markdown/pre.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 
 export default function Pre({ children, ...other }: { children: React.ReactElement }) {
-    if (children.props) {
-        console.log((children.props as {className: string }).className)
-    }
+
+    console.log((children.props as { className: string }).className)
+
     return (
         <div className="code">
             <pre {...other}>{children}</pre>
