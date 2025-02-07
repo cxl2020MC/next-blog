381b5ff1226a4eaa7fb82f31fada0bf963867e11
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2025-02-07 13:31:08 +0000

    test

--- src/components/markdown/pre.tsx
+++ src/components/markdown/pre.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { codeToHtml } from 'shiki'
 
 
-export default function Pre({ children, ...other }: { children: React.ReactElement }) {
+export default async function Pre({ children, ...other }: { children: React.ReactElement }) {
 
-    console.log((children.props as { className: string }).className)
-
+    const lang = (children.props as { className: string }).className?.split('-')[1] || 'plaintest'
+    const code = (children.props as { children: string }).children
+    console.debug(lang)
+    console.debug(code)
+    const html = await codeToHtml(code, {
+        lang: lang,
+        themes: {
+            'light': 'vitesse-light',
+            'dark': 'vitesse-dark'
+        },
+    })
     return (
-        <div className="code">
-            <pre {...other}>{children}</pre>
+        <div className="code" dangerouslySetInnerHTML={{ __html: html }}>
+
         </div>
 
     )
