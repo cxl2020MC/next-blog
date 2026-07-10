2e63f9d29df582dcd3ebba0a2f9671f874e6ef20
cxl2020MC <andamu2019@163.com>
2026-07-10 13:57:01 +0800

    更新依赖

--- src/components/postCard.tsx
+++ src/components/postCard.tsx
@@ -7,12 +7,12 @@ import { PostInfo } from "@/components/ui/post/postInfo"
 import "./css/postCard.css"
 
 
-export default function PostCard({ Article, Index }: { Article: Article, Index?: number|undefined }) {
+export default function PostCard({ Article, Index }: { Article: Article, Index?: number | undefined }) {
     return (
         <div className="card post-card">
             <div className="post-card__image">
                 <Link href={`/posts/${Article.link}`}>
-                    <Image src={Article.cover} alt={Article.title} fill={true} priority={Index===0 ? true : false} />
+                    <Image src={Article.cover} alt={Article.title} fill={true} priority={Index === 0 ? true : false} />
                 </Link>
             </div>
             <div className="post-card__content">
